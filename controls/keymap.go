289e771a811ec31aa32519a4a9f313a06e8d67e0
Iskander Sharipov <quasilyte@gmail.com>
2023-11-26 14:29:16 +0400

    add controls package

---
+++ controls/keymap.go
@@ -0,0 +1,15 @@
+package controls
+
+import (
+	"github.com/quasilyte/ge/input"
+)
+
+const (
+	ActionUnknown input.Action = iota
+
+	ActionMoveAccelerate
+	ActionMoveDecelerate
+	ActionRotateLeft
+	ActionRotateRight
+	ActionDodge
+)
